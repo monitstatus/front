f3cd44f00e2b60890cf8a1d74886e8d2dcacb9a3
Toni Pizà <servomac@gmail.com>
2023-04-06 20:18:38 +0200

    Set FRONT_URL as config

--- src/api/slack.js
+++ src/api/slack.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 
 import {
-  SLACK_CLIENT_ID, SLACK_CLIENT_SECRET
+  FRONT_URL, SLACK_CLIENT_ID, SLACK_CLIENT_SECRET
 } from '@/config'
 
 
@@ -13,7 +13,7 @@ export const slackApi = {
       client_id: SLACK_CLIENT_ID,
       client_secret: SLACK_CLIENT_SECRET,
       code: code,
-      redirect_uri: 'https://app.monitstatus.com/integrations/slack',
+      redirect_uri: `${FRONT_URL}/integrations/slack`,
     }
     console.log("Requesting oauth token with data", formData)
     return axios.post('https://slack.com/api/oauth.v2.access', qs.stringify(formData), { headers: {
@@ -22,6 +22,6 @@ export const slackApi = {
   },
 
   helpers: {
-    authorizationUrl: 'https://slack.com/oauth/v2/authorize?scope=commands%2Cincoming-webhook%2Cusers%3Aread%2Cusers%3Aread.email&redirect_uri=https%3A%2F%2Fapp.monitstatus.com%2Fintegrations%2Fslack&client_id=' + SLACK_CLIENT_ID
+    authorizationUrl: `https://slack.com/oauth/v2/authorize?scope=commands%2Cincoming-webhook%2Cusers%3Aread%2Cusers%3Aread.email&redirect_uri=${encodeURIComponent(FRONT_URL)}%2Fintegrations%2Fslack&client_id=${SLACK_CLIENT_ID}`
   }
 }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+export const FRONT_URL = import.meta.env.VITE_APP_FRONT_URL || 'http://localhost:3000'
 export const API_URL = import.meta.env.VITE_APP_API_URL || 'http://localhost:8000'
 export const STATUS_PAGES_URL = import.meta.env.VITE_APP_STATUS_PAGES_URL || 'http://localhost:8001'
 export const SLACK_CLIENT_ID = import.meta.env.VITE_APP_SLACK_CLIENT_ID || 'slack_client_id'
